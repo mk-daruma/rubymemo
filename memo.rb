841cb3bbf9f93061cf6359cb1e0613d90224c1c0
EC2 Default User <ec2-user@ip-172-31-49-91.ec2.internal>
2022-03-05 15:27:55 +0000

    提出用

--- memo.rb
+++ memo.rb
@@ -1,39 +1,38 @@
 require "csv"
 
-puts "1（新規でメモを作成）2（既存のメモを編集する）"
+# 1を選択した時の関数
+def choice_one()
+  puts "拡張子を除いたファイルを入力してください"
+  title = gets.chomp
+  CSV.open(title + '.csv',"w") do |csv|
+  puts "メモしたい内容を記入してください"
+  puts "完了したらCtrl + Dを押します"
+  memo = gets.chomp
+  csv << [memo]
+  STDIN.read
+  end
+end
+# ２を選択した時の関数
+def choice_two()
+  puts "拡張子を除いたファイルを入力してください"
+  title = gets.chomp
+  CSV.open(title + '.csv',"a") do |csv|
+  puts "メモしたい内容を記入してください"
+  puts "完了したらCtrl + Dを押します"
+  memo = gets.chomp
+  csv << [memo]
+  STDIN.read
+  end
+end
 
+# 実際に動く関数
+puts "1（新規でメモを作成）2（既存のメモを編集する）"
 memo_type = gets.chomp
 
-
 if memo_type == "1"
-  puts "拡張子を除いたファイルを入力してください"
-  memo_type = gets.chomp
-  if memo_type == "test"
-    CSV.open('test.csv',"w")
-    puts "メモしたい内容を記入してください"
-    puts "完了したらCtrl + Dを押します"
-    STDIN.read
-  else
-    puts "#{memo_type}という名前のファイルが存在しません"
-  end
-  
+  choice_one()
 elsif memo_type == "2"
-  puts "拡張子を除いたファイルを入力してください"
-  memo_type = gets.chomp
-  if memo_type == "test"
-    CSV.open('test.csv',"a")
-    puts "メモしたい内容を記入してください"
-    puts "完了したらCtrl + Dを押します"
-    STDIN.read
-  else
-    puts "#{memo_type}という名前のファイルが存在しません"
-  end
-  
+  choice_two()
 else
   puts "#{memo_type}ではなく、1か2を選択してください"
 end
-
-# わからないこと
-# ・終了を押さない限り終わらせる方法(ifに戻る)
-# ・ファイル名を自由に選べるコード
-# ・csvオープンは反応しているから、ちゃんと保存ができている証拠⇨保存をするコードを書けばOK
